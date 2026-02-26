7cce6dbd1326102ee90ca0c15617b749b84af0fc
Gunjan7840 <gunjanshendre892@gmail.com>
2026-02-26 17:48:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const generalRoutes = require('./routes/general.js');
+const authRoutes = require('./routes/auth_users.js');
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+// routes
+app.use('/', generalRoutes);
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
